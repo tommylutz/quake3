c79c11d303630bf919f0b3efdb49a5b4ba6e7357
Thomas Lutz <tommylutz@gmail.com>
2017-03-19 10:58:42 +0000

    Fix linking on Raspberry Pi 3 Model B / Raspbian

--- build_rpi_raspbian.sh
+++ build_rpi_raspbian.sh
@@ -28,11 +28,13 @@ if [ $# -ge 1 ] && [ $1 = clean ]; then
    rm -rf build/*
 fi
 
+# 2017-03-19 [tlutz] Removing -lvmcs_rpc_client from link line for Raspberry Pi 3 Model B (untested with other models)
+
 # sdl not disabled
 make -j4 -f Makefile COPYDIR="$BASEQ3_DIR" ARCH=arm \
 	CC=""$CROSS_COMPILE"gcc" USE_SVN=0 USE_CURL=0 USE_OPENAL=0 \
 	CFLAGS="-DVCMODS_MISC -DVCMODS_OPENGLES -DVCMODS_DEPTH -DVCMODS_REPLACETRIG $INCLUDES" \
-	LDFLAGS="-L"$ARM_LIBS" -L$SDL_LIB -lSDL -lvchostif -lvmcs_rpc_client -lvcfiled_check -lbcm_host -lkhrn_static -lvchiq_arm -lopenmaxil -lEGL -lGLESv2 -lvcos -lrt"
+	LDFLAGS="-L"$ARM_LIBS" -L$SDL_LIB -lSDL -lvchostif -lvcfiled_check -lbcm_host -lkhrn_static -lvchiq_arm -lopenmaxil -lEGL -lGLESv2 -lvcos -lrt"
 
 # copy the required pak3 files over
 # cp "$BASEQ3_DIR"/baseq3/*.pk3 "build/release-linux-arm/baseq3/"
